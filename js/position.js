5834a0f7ab3d66644333e6155cf0b312de67cd22
jameszmartin <jameszwilliammartin@gmail.com>
2015-04-20 12:33:37 +1000

    Create position.js

---
+++ js/position.js
@@ -0,0 +1,8 @@
+var domElements = require('./domElements');
+
+var position = {
+    x: domElements.slideContainer.offsetLeft,
+    y: domElements.slideContainer.offsetTop
+};
+
+module.exports = position;
