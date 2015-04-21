811b41fc0cdb5c7bd73cb4cdf179e813d34d6cf2
jameszmartin <jameszwilliammartin@gmail.com>
2015-04-21 12:15:41 +1000

    Update domElements.js

--- js/domElements.js
+++ js/domElements.js
@@ -1,10 +1,10 @@
 var domElements = {
-	frame: undefined,
-	slideContainer: undefined,
-	prevCtrl: undefined,
-	nextCtrl: undefined,
-    slidesWidth: undefined,
-    frameWidth: undefined
+  frame: undefined,
+  slideContainer: undefined,
+  prevCtrl: undefined,
+  nextCtrl: undefined,
+  slidesWidth: undefined,
+  frameWidth: undefined
 };
 
 module.exports = domElements;
